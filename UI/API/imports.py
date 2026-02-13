814d7ac3834ccc0ee1f678bed8dd8292a2d72fc4
Kieran Selby <K.Selby@2023.ljmu.ac.uk>
2026-02-13 04:39:33 +0000

    new colours

--- UI/API/imports.py
+++ UI/API/imports.py
@@ -1,9 +1,10 @@
 import pygame
 import sys
+import json
 from threading import Timer
 
 # Frames per second setting
-FPS = 60
+FPS = 120
 fpsClock = pygame.time.Clock()
 
 # Sets up the colors
@@ -11,14 +12,17 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 grey = (160, 160, 160)
-dot_colour = (60, 60, 60)
+grey_2 = (60, 60, 60)
 background_colour = (10, 10, 15)
 text_colour_red = (206, 46, 30)
 box_colour = (18, 22, 30)
 box_hover = (28, 34, 45)
 red_box = (200, 40, 40)
+box_colour_2 = (20, 20, 20)
+box_hover_2 = (40, 40, 40)
 blue = (40, 90, 200)   # Blue
 green = (40, 170, 90)    # Green
 purple = (128, 0, 128)
+yellow = (255, 255, 0)
 
 font_name = "data/UI/F1_Fonts/Formula1/Formula1-Bold.ttf"
